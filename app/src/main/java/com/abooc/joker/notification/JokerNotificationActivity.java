cf75c2be5f5a197767e227ca5bf1485a06887ed8
大宇 <allnet@live.cn>
2017-04-24 14:47:09 +0800

    增加：添加项目代码；

---
+++ app/src/main/java/com/abooc/joker/notification/JokerNotificationActivity.java
@@ -0,0 +1,13 @@
+package com.abooc.joker.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokerNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joker_notification);
+    }
+}
